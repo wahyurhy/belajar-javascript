5c6989a57dd5348557cc0de039e8f9f506f639a7
wahyurhy <wahyurahayu82@gmail.com>
2024-10-17 08:47:02 +0700

    materi paradigma berbasis objek

--- javascript-dicoding/paradigma-berbasis-objek.js
+++ javascript-dicoding/paradigma-berbasis-objek.js
@@ -1,12 +1,34 @@
-function Person(name, age) {
-    this.name = name;
-    this.age = age;
+// function Person(name, age) {
+//     this.name = name;
+//     this.age = age;
+// }
+
+// Person.prototype.eat = function() {
+//     console.log(`${this.name} is eating`);
+// }
+
+// const person1 = new Person('Wahyu', 25);
+
+// person1.eat();
+
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    eat() {
+        console.log(`${this.name} is eating.`);
+    }
 }
 
-Person.prototype.eat = function() {
-    console.log(`${this.name} is eating`);
-}
+console.log(`Tipe dari class Person adalah ${typeof Person}`);
 
-const person1 = new Person('Wahyu', 25);
+const person1 = new Person('Alice', 30);
+const person2 = new Person('Bob', 25);
+
+console.log(person1.name);
+console.log(person2.name);
 
 person1.eat();
+person2.eat();
